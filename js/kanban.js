defe05f3c2e25140dc07adbf1b0dd3d459267d1a
Svitlana Galianova <Svitlana_Galianova@manulife.com>
2020-09-18 19:32:44 -0400

    Kanban board events: 6/15

--- js/kanban.js
+++ js/kanban.js
@@ -16,6 +16,19 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  let item = document.createElement("div")
+  item.classList.add("item")
+  item.id = "item-" + order
+  item.draggable = true
+
+  item.addEventListener("dragstart", event => event.dataTransfer.setData("text", event.target.id))
+  item.addEventListener("dragend", event => event.dataTransfer.clearData())
+
+  let input = document.createElement("input")
+  item.appendChild(input)
+
+  let save_btn = document.createElement("button")
+  save_btn.innerHTML = "Save"
 };
 
 document.querySelectorAll('.drop').forEach(element => {
